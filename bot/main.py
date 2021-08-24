8ca7cde1b24b3f9c5e8cd14fe8bbaa75f6fa56d2
Brian Sang <brian.sang@sendbird.com>
2021-08-24 11:06:09 +0900

    update

--- bot/main.py
+++ bot/main.py
@@ -43,7 +43,7 @@ def index(request):
                 thread = threading.Thread(target=sendAdminMessage, args=(category, app_id, channel_url, message))
                 thread.start()
 
-        time.sleep(5)
+        time.sleep(3)
         return HttpResponse('Hello bot!')
 
 
